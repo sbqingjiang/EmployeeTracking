4d95ad15ade097dc01b2cc9784c54681724bf1fa
yinqingjiang <Qingjiang.yin@stonybrook.edu>
2017-07-03 18:04:11 -0500

    create fragments package

--- app/src/main/java/com/tracking/employeetracking/HomePage.java
+++ app/src/main/java/com/tracking/employeetracking/HomePage.java
@@ -10,14 +10,18 @@ import android.support.v7.widget.Toolbar;
 import com.tracking.employeetracking.fragments.schedule.MyShiftsFragmentHost;
 
 public class HomePage extends AppCompatActivity {
+    private ActionBarDrawerToggle toggle;
     private Toolbar toolbar;
     private NavigationView mNavigationView;
-    private ActionBarDrawerToggle toggle;
     private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        initViews();
+        setActionBar();
+        setDrawerToggle();
 
         if (findViewById(R.id.home_container)!=null) {
             if (savedInstanceState != null)
@@ -27,21 +31,24 @@ public class HomePage extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().add(R.id.home_container, host).addToBackStack(null).commit();
         }
 
-//        toolbar= (Toolbar) findViewById(R.id.toolBar);
-//        toolbar.inflateMenu(R.menu.navi_menu);
-//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(MenuItem item) {
-//                Toast.makeText(getApplicationContext(),"TEST",Toast.LENGTH_LONG).show();
-//                return false;
-//            }
-//        });
-//
-//        setSupportActionBar(toolbar);
-//        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mNavigationView= (NavigationView) findViewById(R.id.nav_view);
-//
-
-
     }
+    private void initViews()
+    {
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        mNavigationView= (NavigationView) findViewById(R.id.nav_view);
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+    private void setActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setDrawerToggle() {
+        toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(toggle);
+        /*同步drawerlayout的状态*/
+        toggle.syncState();
+    }
+
+
 }
---
+++ app/src/main/java/com/tracking/employeetracking/fragments/schedule/test.java
@@ -0,0 +1,8 @@
+package com.tracking.employeetracking.fragments.schedule;
+
+/**
+ * Created by yinqingjiang on 7/3/17.
+ */
+
+public class test {
+}
