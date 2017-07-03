758edbc25712a1c3a6f4ee1e000d61bf2e0d61fc
yinqingjiang <qingjiang.yin@stonybrook.edu>
2017-07-03 11:24:34 -0500

    create fragments package

--- app/src/main/java/com/tracking/employeetracking/HomePage.java
+++ app/src/main/java/com/tracking/employeetracking/HomePage.java
@@ -1,20 +1,47 @@
 package com.tracking.employeetracking;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import com.tracking.employeetracking.fragments.schedule.MyShiftsFragmentHost;
 
 public class HomePage extends AppCompatActivity {
     private Toolbar toolbar;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        toolbar= (Toolbar) findViewById(R.id.toolBar);
-        setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        if (findViewById(R.id.home_container)!=null) {
+            if (savedInstanceState != null)
+                return;
+
+            MyShiftsFragmentHost host=new MyShiftsFragmentHost();
+            getSupportFragmentManager().beginTransaction().add(R.id.home_container, host).addToBackStack(null).commit();
+        }
+
+//        toolbar= (Toolbar) findViewById(R.id.toolBar);
+//        toolbar.inflateMenu(R.menu.navi_menu);
+//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                Toast.makeText(getApplicationContext(),"TEST",Toast.LENGTH_LONG).show();
+//                return false;
+//            }
+//        });
+//
+//        setSupportActionBar(toolbar);
+//        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mNavigationView= (NavigationView) findViewById(R.id.nav_view);
+//
+
+
     }
 }
