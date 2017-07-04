801728c369ce7f953c474c20d7a0177bb13cd5c9
yinqingjiang <Qingjiang.yin@stonybrook.edu>
2017-07-04 12:12:37 -0500

    create Login activity

--- app/src/main/java/com/tracking/employeetracking/HomePage.java
+++ app/src/main/java/com/tracking/employeetracking/HomePage.java
@@ -2,12 +2,12 @@ package com.tracking.employeetracking;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import com.tracking.employeetracking.fragments.schedule.MyShiftsFragmentHost;
+import android.view.MenuItem;
 
 public class HomePage extends AppCompatActivity {
     private ActionBarDrawerToggle toggle;
@@ -20,16 +20,19 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         initViews();
+        hideScrollBar();
         setActionBar();
         setDrawerToggle();
 
-        if (findViewById(R.id.home_container)!=null) {
-            if (savedInstanceState != null)
-                return;
+        setListener();
 
-            MyShiftsFragmentHost host=new MyShiftsFragmentHost();
-            getSupportFragmentManager().beginTransaction().add(R.id.home_container, host).addToBackStack(null).commit();
-        }
+//        if (findViewById(R.id.home_container)!=null) {
+//            if (savedInstanceState != null)
+//                return;
+//
+//            MyShiftsFragmentHost host=new MyShiftsFragmentHost();
+//            getSupportFragmentManager().beginTransaction().add(R.id.home_container, host).addToBackStack(null).commit();
+//        }
 
     }
     private void initViews()
@@ -42,12 +45,35 @@ public class HomePage extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    private void hideScrollBar() {
+        mNavigationView.getChildAt(0).setVerticalScrollBarEnabled(false);
+    }
 
     private void setDrawerToggle() {
         toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(toggle);
         /*同步drawerlayout的状态*/
         toggle.syncState();
+    }
+    private void setListener() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.schedule:
+                        break;
+                    case R.id.dashboard:
+                        break;
+                    case R.id.request:
+                        break;
+//                    case R.id.item_4:
+//                        finish();
+//                        break;
+                }
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
 
 
--- app/src/main/java/com/tracking/employeetracking/fragments/schedule/test.java
+++
@@ -1,8 +0,0 @@
-package com.tracking.employeetracking.fragments.schedule;
-
-/**
- * Created by yinqingjiang on 7/3/17.
- */
-
-public class test {
-}
