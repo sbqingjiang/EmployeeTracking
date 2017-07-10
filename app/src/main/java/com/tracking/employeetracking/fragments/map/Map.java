31997327fa5993dec0b2e3340cd1300584aad470
yinqingjiang <Qingjiang.yin@stonybrook.edu>
2017-07-09 22:44:19 -0500

    finish map circle custom dialog add testcases

--- app/src/main/java/com/tracking/employeetracking/fragments/map/Map.java
+++ app/src/main/java/com/tracking/employeetracking/fragments/map/Map.java
@@ -1,13 +1,16 @@
 package com.tracking.employeetracking.fragments.map;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +26,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.tracking.employeetracking.R;
@@ -32,6 +37,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import static com.tracking.employeetracking.R.id.my_map;
+import static com.tracking.employeetracking.R.layout.dialog;
 
 /**
  * Created by yinqingjiang on 7/5/17.
@@ -44,8 +50,8 @@ public class Map extends Fragment implements OnMapReadyCallback {
     private String endTime;
     private String dateTime;
     private String url = "http://rjtmobile.com/aamir/emp-mgt-sys/apps/shift_info.php?mobile=55565454";
-    private StringBuilder sb = new StringBuilder("http://rjtmobile.com/aamir/emp-mgt-sys/apps/clock_in.php? mobile= 55565454&notes=sorry I m late I missed my bus");
-    private String url_shift="http://rjtmobile.com/aamir/emp-mgt-sys/apps/emp_in_shift.php?shiftID=1";
+    private StringBuilder sb = new StringBuilder("http://rjtmobile.com/aamir/emp-mgt-sys/apps/clock_in.php? mobile= 55565454&notes=");
+    private String url_shift = "http://rjtmobile.com/aamir/emp-mgt-sys/apps/emp_in_shift.php?shiftID=1";
 
     @Nullable
     @Override
@@ -77,9 +83,15 @@ public class Map extends Fragment implements OnMapReadyCallback {
                 .title("Marker in Office"));
 //        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        Circle circle = googleMap.addCircle(new CircleOptions()
+                .center(sydney)
+                .radius(1000)
+                .strokeColor(Color.RED)
+                .fillColor(Color.TRANSPARENT));
+
         CameraUpdate center =
                 CameraUpdateFactory.newLatLng(sydney);
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(10);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(12);
 
         googleMap.moveCamera(center);
         googleMap.animateCamera(zoom);
@@ -94,17 +106,17 @@ public class Map extends Fragment implements OnMapReadyCallback {
                     JSONArray array = jsonObject.getJSONArray("Shift Information");
                     JSONObject obj = array.getJSONObject(0);
                     startTime = obj.getString("ShiftStartTime");
-                    tv_shift.setText(startTime.substring(0,5));
+                    tv_shift.setText(startTime.substring(0, 5));
 
                     endTime = obj.getString("ShiftEndTime");
-                    Toast.makeText(getContext(), endTime, Toast.LENGTH_SHORT).show();
+                    // Toast.makeText(getContext(), endTime, Toast.LENGTH_SHORT).show();
                     tv_shift.append("p-");
-                    tv_shift.append(endTime.substring(0,5));
+                    tv_shift.append(endTime.substring(0, 5));
                     tv_shift.append("p");
 
                     dateTime = obj.getString("ShiftDate");
                     tv_date.setText(dateTime);
-                    Toast.makeText(getContext(), startTime, Toast.LENGTH_LONG).show();
+                    // Toast.makeText(getContext(), startTime, Toast.LENGTH_LONG).show();
                     ;
                     //endTime = info.getString("ShiftEndTime");
                     //date = info.getString("ShiftDate");
@@ -149,25 +161,53 @@ public class Map extends Fragment implements OnMapReadyCallback {
     }
 
     public void showDialog() {
-        final AlertDialog.Builder dialog=new AlertDialog.Builder(getContext());
-        dialog.setTitle("Would you like to clock in ? ");
-        dialog.setMessage("2:00 -800");
-        dialog.setPositiveButton("CLOCK IN",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //...To-do
-                    }
-                });
-        dialog.setNegativeButton("CANCEL",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //...To-do
-                    }
-                });
-        dialog.show();
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View customTitleView = inflater.inflate(R.layout.dialog_title, null);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setCustomTitle(customTitleView);
 
+        final View dialogView = LayoutInflater.from(getActivity())
+                .inflate(dialog, null);
+        builder.setView(dialogView);
+        EditText editText=(EditText)dialogView.findViewById(R.id.editText);
+        Button cancel=(Button) dialogView.findViewById(R.id.cancel);
+        Button clockin=(Button)dialogView.findViewById(R.id.checkin);
+        final AlertDialog dialog = builder.create();
+
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dialog.dismiss();
+            }
+        });
+        clockin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+        dialog.show();
+    }
+
+    public void submit()
+    {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(stringRequest);
     }
 
 }
